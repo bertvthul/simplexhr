0398b6fea9904b8d1bbf00c8586c2724b42a60a9
Bert van t Hul <bertvthul@gmail.com>
2019-12-16 19:43:23 +0100

    append and prepend html

--- src/js/simplexhr.js
+++ src/js/simplexhr.js
@@ -1,82 +1,90 @@
 $(function() {
 
-	$.ajaxSetup({
-	    headers: {
-	        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-	    }
-	});
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
 
-	/*
-	XHR post on data
-	<span class="btn btn-secondary" data-simplexhr="{{ route('xhrRequest.post') }}" data-values="{'test': 'hoi'}">Test</span>
-	*/
-	$(document).on('click', "[data-simplexhr]:not('.xhr-pending'):not(form)", function(e){
-		$(e.target).addClass('xhr-pending');
-		if (json_values = $(this).data('values')) {
-			json_values = json_values.replace(new RegExp("'", 'g'), '"');
-		}
-		var data = json_values ? JSON.parse(json_values) : {};
-		data.call = $(this).data('simplexhr');
-	    xhrCall(data, e);
-	});
+    /*
+    XHR post on data
+    <span class="btn btn-secondary" data-simplexhr="{{ route('xhrRequest.post') }}" data-values="{'test': 'hoi'}">Test</span>
+    */
+    $(document).on('click', "[data-simplexhr]:not('.xhr-pending'):not(form)", function(e){
+        $(e.target).addClass('xhr-pending');
+        if (json_values = $(this).data('values')) {
+            json_values = json_values.replace(new RegExp("'", 'g'), '"');
+        }
+        var data = json_values ? JSON.parse(json_values) : {};
+        data.call = $(this).data('simplexhr');
+        xhrCall(data, e);
+    });
 
-	$(document)
-		.on('submit', "form[data-simplexhr]:not('.xhr-pending')", function(e){
-			e.preventDefault();
-			formSubmit(e.target);
-		})
-		.on('submit', "form[data-simplexhr][onchange]", function(e) {
-			e.preventDefault();
-		}).on('change', "form[data-simplexhr][onchange]:not('.xhr-pending')", function(e) {
-			e.preventDefault();
-			formSubmit($(e.target).parent('form'));
-		});
+    $(document)
+        .on('submit', "form[data-simplexhr]:not('.xhr-pending')", function(e){
+            e.preventDefault();
+            formSubmit(e.target);
+        })
+        .on('submit', "form[data-simplexhr][onchange]", function(e) {
+            e.preventDefault();
+        }).on('change', "form[data-simplexhr][onchange]:not('.xhr-pending')", function(e) {
+            e.preventDefault();
+            formSubmit($(e.target).parent('form'));
+        });
 
-	function formSubmit(form) {
-		$(form).addClass('xhr-pending');
-		var data = objectifyForm($(form).serializeArray());
-		data.call = $(form).data('simplexhr');
-	    xhrCall(data, form);
-	    return false;
-	}
+    function formSubmit(form) {
+        $(form).addClass('xhr-pending');
+        var data = objectifyForm($(form).serializeArray());
+        data.call = $(form).data('simplexhr');
+        xhrCall(data, form);
+        return false;
+    }
 
-	function xhrCall(data, e) {
-		var object = (e.target == undefined ? e : e.target);
-		console.log(data);
-		$.ajax({
-			type:'POST',
-			url:'/simplexhrRequest',
-			data:data,
-			success:function(data){
-				$(object).removeClass('xhr-pending');
-				
-				if ('html' in data && typeof data.html === 'object') {
-					Object.keys(data.html).forEach(function (key) {
-						if ($(key)[0]) {
-							$(key).html(data.html[key]);
-						}
-					});
-				}
+    function xhrCall(data, e) {
+        var object = (e.target == undefined ? e : e.target);
+        $.ajax({
+            type:'POST',
+            url:'/simplexhrRequest',
+            data:data,
+            success:function(data){
+                $(object).removeClass('xhr-pending');
+                
+                if ('html' in data && typeof data.html === 'object') {
+                    Object.keys(data.html).forEach(function (key) {
+                        var type = 'replace';
+                        if (key.indexOf(':') != -1) {
+                            type = key.substring(key.indexOf(':') + 1, key.length);
+                            key = key.substring(0, key.indexOf(':'));
+                        }
+                        if ($(key)[0] && type == 'replace') {
+                            $(key).html(data.html[key]);
+                        } else if ($(key)[0] && type == 'append') {
+                            $(key).append(data.html[key + ':append']);
+                        } else if ($(key)[0] && type == 'prepend') {
+                            $(key).prepend(data.html[key + ':prepend']);
+                        }
+                    });
+                }
 
-				if (typeof $.xhrCustomCallback === 'function') {
-					$.xhrCustomCallback(data);
-				}
-	 		}
-	    });
-	}
+                if (typeof $.xhrCustomCallback === 'function') {
+                    $.xhrCustomCallback(data);
+                }
+            }
+        });
+    }
 
-	function objectifyForm(formArray) {
-		var returnArray = {};
+    function objectifyForm(formArray) {
+        var returnArray = {};
 
-		if (!Array.isArray(formArray)) {
-			return returnArray;
-		}
-		
-		for (var i = 0; i < formArray.length; i++){
-			returnArray[formArray[i]['name']] = formArray[i]['value'];
-		}
+        if (!Array.isArray(formArray)) {
+            return returnArray;
+        }
+        
+        for (var i = 0; i < formArray.length; i++){
+            returnArray[formArray[i]['name']] = formArray[i]['value'];
+        }
 
-		return returnArray;
-	}
+        return returnArray;
+    }
 
 });
